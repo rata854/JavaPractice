841576f6ff1fe9afeaf050bfe02110c87ef6beb6
rata854 <nitiham2006@gmail.com>
2021-08-12 11:15:27 +0900

    FirstCommit

---
+++ JavaSample/src/Chapter04.java
@@ -0,0 +1,23 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "Karasu";
+		System.out.println(str1);
+		
+		str1 = "食";
+		System.out.println(str1);
+
+		final int lucky = 7;
+		System.out.println(lucky);
+		
+        final int LUCKY_NUMBER = 7;
+        String str = "ウェブ　キャンプ之助";
+        System.out.println(str);
+        str = "少年よ大志を抱け";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+	}
+}
+
+
