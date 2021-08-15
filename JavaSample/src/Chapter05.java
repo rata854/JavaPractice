74afc28b95ae23c146659e96a2b9694fe9efbe2a
rata854 <nitiham2006@gmail.com>
2021-08-15 09:04:23 +0900

    [Finish]Chapter05

---
+++ JavaSample/src/Chapter05.java
@@ -0,0 +1,38 @@
+
+public class Chapter05 {
+	public static void main (String[] args) {
+		  long test1 = 550;
+		  double test2 = 12.5d;
+		  boolean test3 = true;
+		  System.out.println(test1);
+		  System.out.println(test2);
+		  System.out.println(test3);
+		  
+		  Float test4 = 0.5f;
+		  Integer test5 = 10;
+		  Character test6 = 'c';
+		  System.out.print(test4);
+		  System.out.print(test5);
+		  System.out.print(test6);
+		  
+		  int[] array = {10, 20, 30, 40, 50};
+		  System.out.print(array[3]);
+		  
+//        long l = 10000000000L;
+//        System.out.println(l);
+//        double d = 3.14;
+//        System.out.println(d);
+//        boolean b = true;
+//        System.out.println(b);
+//
+//        Integer i = 100;
+//        System.out.println(i);
+//        Float f = 1.4142f;
+//        System.out.println(f);
+//        Character c = 'う';
+//        System.out.println(c);
+//
+//        int[] array = { 0, 10, 20, 30, 40 };
+//        System.out.println(array[3]);
+	}
+}
