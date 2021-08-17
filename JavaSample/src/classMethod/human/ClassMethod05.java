860bf37b2a49b0458b25553aee498ccdccb72882
rata854 <nitiham2006@gmail.com>
2021-08-18 07:52:31 +0900

    [Finish]Chapter9

--- JavaSample/src/classMethod/human/ClassMethod05.java
+++ JavaSample/src/classMethod/human/ClassMethod05.java
@@ -4,6 +4,9 @@ public class ClassMethod05 {
 	public static void main(String[] args) {
 		Human05 yamada = new Human05();
 		
+        //nameとageはprivateな変数なので直接参照はできない
+        // ☓ System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
+		
 		System.out.println("名前は" + yamada.getName() + "で、年齢は" + yamada.getAge() + "です。");
 	}
 
