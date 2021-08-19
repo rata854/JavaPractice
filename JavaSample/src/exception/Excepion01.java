6fbc7eea5c8dcc7c584d9c1fe759d003418db146
rata854 <nitiham2006@gmail.com>
2021-08-19 11:34:52 +0900

    [Start]Chapter10

---
+++ JavaSample/src/exception/Excepion01.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class Excepion01 {
+	public static void main(String[] args) {
+		System.out.println("100 ÷ 0は？");
+		int result = 100 / 0;
+		System.out.println("計算結果" + result);
+		System.out.println("プログクラム終了");
+	}
+
+}
