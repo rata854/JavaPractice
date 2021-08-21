040adcfb87af9bde7365341fa8a6b720832edc28
rata854 <nitiham2006@gmail.com>
2021-08-21 11:38:31 +0900

    [Finish]Capter12

--- JavaSample/src/capsule/Capsule01.java
+++ JavaSample/src/capsule/Capsule01.java
@@ -9,7 +9,7 @@ public class Capsule01 {
 		human1.name = "今田";
 		human1.age = 70;
 		System.out.println("名前は" + human1.name + ", 年齢は" + human1.age + "です。");
-	
+
 	    HumanCapsule human2 = new HumanCapsule("小林", 32);
 	    System.out.println("名前は" + human2.getName() + ", 年齢は" + human2.getAge() + "です。");
 	    human2.setName("佐々木");
