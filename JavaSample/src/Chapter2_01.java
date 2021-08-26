4bfe45c43189fa3f9fbbcbf5e4c36b8a32680e81
rata854 <nitiham2006@gmail.com>
2021-08-26 11:14:43 +0900

    [Finish]Chapter2_1

---
+++ JavaSample/src/Chapter2_01.java
@@ -0,0 +1,8 @@
+
+public class Chapter2_01 {
+	public static void main(String[] args) {
+		Human human = new Human();
+		Human.HumanLeg leg = human.new HumanLeg();
+		leg.kick();
+	}
+}
---
+++ JavaSample/src/Human.java
@@ -0,0 +1,8 @@
+
+public class Human {
+	public class HumanLeg {
+		public void kick() {
+			System.out.println("丸ケリータ");
+		}
+	}
+}
